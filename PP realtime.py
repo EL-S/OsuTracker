ef33118a9bf32aedd72893f06bf6ebe646aa9ef5
EL-S <31227083+EL-S@users.noreply.github.com>
2018-11-22 23:38:06 +1100

    Refactored Code
    Implemented a better data system, a prompt to validate usernames at the start, automatic username tracking during the main loop and finally, storage of both usernames and user_ids with the pp data in a single file.

--- PP realtime.py
+++ PP realtime.py
@@ -3,40 +3,83 @@ from bs4 import BeautifulSoup
 import json
 import time
 
-#change
-config_name = "config.txt"
-pp_data_name = "pp_data.txt"
-pp_threshold = 0 #only show pp changes above this amount
-wait = 1 #seconds
+data_name = "data.txt"
+pp_threshold = 0
+wait = 1
 
-#don't change
-userids_to_add = []
-usernames = []
-config_usernames = []
-pp_data = {}
+user_data = []
+data = []
 prompt = "n"
 flag = False
 pp = 0
 pp_old = 0
 
 def init():
+    global user_data
     try:
-        read_config()
-        add_new_users_prompt()
-        add_users()
-        update_userlist()
-        initialise_values()
-        read_pp_data()
-    except:
-        with open(config_name, "w") as file:
+        read_data()
+        update_user_names()
+        add_new_users()
+        write_data()
+        print("Now Tracking...")
+    except IOError:
+        print("Creating Data File...")
+        with open(data_name, "w") as file:
             file.write("")
         init()
 
-def initialise_values():
-    for username in usernames:
-        pp_data[username] = 0 #to prevent null errors
+def read_data():
+    global user_data
+    user_data = [] #clear all data
+    with open(data_name, "r+") as file:
+        for line in file:
+            data = line.strip().split(",")
+            #user_id = data[0]
+            #user_name = data[1]
+            #pp = data[2]
+            user_data.append(data)
 
-def get_username(user_id):
+def write_data():
+    global user_data
+    with open(data_name, "w") as file:
+        for data_packet in user_data:
+            data = ",".join(data_packet)+"\n"
+            file.write(data)
+
+def update_user_names(): #might not be needed at all, already done in the loop
+    global user_data
+    if user_data != []:
+        flag_1 = False
+        prompt_update = "n"
+        while (prompt_update != "y") and (flag_1 == False):
+            prompt_update = input("Validate Usernames?: ").lower()
+            if (prompt_update == "yes"):
+                prompt_update = "y"
+            elif (prompt_update == "no"):
+                prompt_update = "n"
+            if (prompt_update == "y") or (prompt_update == "n"):
+                flag_1 = True
+        new_user_names = []
+        if prompt_update == "y":
+            print("Validating Usernames...")
+            for data_packet in user_data:
+                    user_id = data_packet[0]
+                    old_user_name = data_packet[1]
+                    new_user_name,user_id,pp = get_user_data(user_id) #get new username for human readability
+                    data_packet[1] = new_user_name
+                    if new_user_name != None:
+                        new_user_names.append(new_user_name)
+                        if new_user_name != old_user_name:
+                            print(old_user_name,"changed their username to",new_user_name+"!")
+                        print("Validated {}".format(new_user_name))
+                    #else maybe remove the person because their account is gone?
+        else:
+            for data_packet in user_data:
+                new_user_names.append(data_packet[1])
+        if new_user_names != []:
+            print("Currently Tracking: "+", ".join(new_user_names))
+
+def get_user_data(user_id):
     global wait
     try:
         time.sleep(wait)
@@ -47,59 +90,33 @@ def get_username(user_id):
             data_profile = soup.find("script", attrs={"id": "json-user"}).text.strip()
             json_data = json.loads(data_profile)
             username_on_page = json_data["username"]
-            return username_on_page
+            userid_on_page = json_data["id"]
+            try:
+                pp = float(json_data['statistics']['pp'])
+            except:
+                #never submitted a score
+                pp = float(0)
+            return [username_on_page,userid_on_page,pp]
         except Exception as e:
-            print(e)
             print("Invalid user: {}".format(user_id))
             return None
     except:
-        get_username(user_id)
+        get_user_data(user_id)
 
-def get_userid(username):
+def request_url(url): #to make getting proxies and custom headers easy
     global wait
+    #proxies = get_proxies() #maybe if it has been 10 minutes since the last one?
     try:
-        time.sleep(wait)
-        url = 'https://osu.ppy.sh/u/'+str(username)
-        data = request_url(url)
-        soup = BeautifulSoup(data, "lxml")
-        try:
-            #print(soup)
-            data_profile = soup.find("script", attrs={"id": "json-user"}).text.strip()
-            json_data = json.loads(data_profile)
-            id_on_page = json_data["id"]
-            return id_on_page
-        except Exception as e:
-            print(e)
-            print("Invalid user: {}".format(username))
-            return None
+        req = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
+        data = req.text
+        return data
     except:
-        get_userid(username)
+        time.sleep(wait)
+        request_url(url)
 
-
-def read_config():
-    global config_usernames
-    with open(config_name, "r+") as file:
-        for line in file:
-            stripped_line = line.strip()
-            user_id = stripped_line.split(",")[0]
-            if user_id not in config_usernames:
-                config_usernames.append(user_id)
-    if config_usernames != []:
-        print("Validating Usernames...")
-    print_usernames = []
-    for user_id_name in config_usernames:
-            username = get_username(user_id_name) #get username for human readability, especially if it's a user_id
-            if username != None:
-                print_usernames.append(username)    
-    if prompt == "y":
-        print("Now Tracking: "+", ".join(print_usernames))
-    else:
-        if print_usernames != []:
-            print("Currently Tracking: "+", ".join(print_usernames))
-
-def add_new_users_prompt():
+def add_new_users():
     try:
-        global userids_to_add, prompt, flag
+        global prompt, flag, user_data
         while (prompt != "y") and (flag == False):
             prompt = input("Add new users to track? (y/n): ").lower()
             if (prompt == "yes"):
@@ -111,104 +128,68 @@ def add_new_users_prompt():
         if prompt == "y":
             users = input("New Users (eg. username1,username2): ")
             users_to_add = users.split(",")
+            userdata_to_add = []
             for user in users_to_add:
-                stripped_user = str(user).strip()
-                user_id = get_userid(stripped_user)
-                if user_id != None:
-                    userids_to_add.append(str(user_id).strip())
+                user_name = str(user).strip()
+                data = get_user_data(user_name)
+                if data != None:
+                    user_name,user_id,pp = data[0],data[1],data[2]
+                    print("Acquired {}'s Data".format(user_name))
+                    if user_id != None:
+                        userdata_to_add.append([str(user_id).strip(),str(user_name),str(pp)])
+                    else:
+                        print("Error finding UserID for user: {}".format(user_name))
+            currently_tracked = []
+            for data_packet in user_data:
+                currently_tracked.append(data_packet[1])
+            for data_packet in userdata_to_add:
+                if data_packet[1] in currently_tracked:
+                    print("Already Tracking {}".format(data_packet[1])) #do nothing
                 else:
-                    print("Error finding UserID for user: {}".format(stripped_user))
+                    user_data.append(data_packet) #add the user to the user_data
+                    print("Validated {}".format(data_packet[1]))
+            if user_data != []:
+                user_names = []
+                for data_packet in user_data:
+                    user_names.append(data_packet[1])
+                print("Now Tracking: "+", ".join(user_names))
         else:
-            pass
+            pass #no users to add
     except Exception as e:
-        print("yeet",e)
+        print("error2",e)
 
-def add_users():
-    global userids_to_add,prompt
-    with open(config_name, "a") as file:
-        for username in userids_to_add:
-            if username not in config_usernames:
-                file.write(username.strip()+"\n")
-    if prompt == "y":
-        read_config()
-    
-
-def update_userlist():
-    global usernames
-    with open(config_name, "r+") as file:
-        for line in file:
-            if line not in usernames:
-                usernames.append(line.strip())
-
-def read_pp_data():
-    global pp_data_name, pp_data
-    try:
-        with open(pp_data_name, "r+") as file:
-            for line in file:
-                user_data = line.split(",")
-                username = user_data[0].strip()
-                pp = user_data[1].strip()
-                pp_data[username] = float(pp)
-    except:
-        with open(pp_data_name, "w") as file:
-            file.write("")
-
-def write_pp_data():
-    with open(pp_data_name, "w") as file:
-        for data in pp_data.items():
-            csv_data = data[0]+","+str(data[1])+"\n"
-            file.write(csv_data)
-    
-def request_url(url):
-    global wait
-    try:
-        req = requests.get(url, headers={"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"})
-        data = req.text
-        #print(data)
-        return data
-    except Exception as e:
-        print("yeet2",e)
-        time.sleep(wait)
-        request_url(url)
-
-init()
-
-print("Now tracking for live changes!")
-while True:
-    for username in usernames:
-        try:
-            time.sleep(wait)
-            url = 'https://osu.ppy.sh/u/'+str(username)
-            data = request_url(url)
-            soup = BeautifulSoup(data, "lxml")
-            try:
-                data_profile = soup.find("script", attrs={"id": "json-user"}).text.strip()
-                json_data = json.loads(data_profile)
-                username_on_page = json_data["username"]
-                id_on_page = json_data["id"]
-            except Exception as e:
-                print(e)
-                data_profile = None
-            if data_profile != None:
-                pp_old = float(pp_data[username])
-                pp = float(json_data['statistics']['pp'])
+def main():
+    for data_packet in user_data:
+        user_id = data_packet[0]
+        old_pp = data_packet[2]
+        data = get_user_data(user_id)
+        if data != None:
+            user_name,user_id,new_pp = data[0],data[1],data[2]
+            if user_name != data_packet[1]:
+                print(data_packet[1],"changed their username to",user_name+"!")
+                data_packet[1] = str(user_name)
+            if user_id != None:
                 try:
                     time_current = time.strftime("%I:%M:%S %p %d/%m/%y", time.localtime(int(time.time())))
                 except:
                     pass
-                pp_change = float(pp)-float(pp_old)
-                pp = "{0:.2f}".format(round(float(pp),2))
-                pp_old = "{0:.2f}".format(round(float(pp_old),2))
-                if (pp != pp_old) and ((pp_change >= pp_threshold) or (pp_threshold == 0)):
-                    pp_data[username] = pp
-                    pp_change = "{0:.2f}".format(round(float(pp_change),2))
+                pp_new_rounded = "{0:.2f}".format(round(float(new_pp),2))
+                pp_old_rounded = "{0:.2f}".format(round(float(old_pp),2))
+                pp_change = float(pp_new_rounded)-float(pp_old_rounded)
+                if (pp_new_rounded != pp_old_rounded) and ((pp_change >= pp_threshold) or (pp_threshold == 0)):
+                    data_packet[2] = str(new_pp)
+                    pp_change = float("{0:.2f}".format(round(float(pp_change),2)))
                     if (pp_change < 0):
-                        print(username_on_page+":","NEW PP("+str(pp)+")"+" -> ""RAW PP LOSS("+str(abs(pp_change))+")",time_current)
+                        print(user_name+":","NEW PP("+str(pp_new_rounded)+")"+" -> ""RAW PP LOSS("+str(abs(pp_change))+")",time_current)
                     else:
-                        print(username_on_page+":","NEW PP("+str(pp)+")"+" -> ""RAW PP GAIN("+str(pp_change)+")",time_current)
-            else:
-                print("Username {} does not exist! Name change?".format(username))
-                usernames.remove(username)
-        except Exception as e:
-            print("yeet3",e,pp,pp_old)
-    write_pp_data()
+                        print(user_name+":","NEW PP("+str(pp_new_rounded)+")"+" -> ""RAW PP GAIN("+str(pp_change)+")",time_current)
+        else:
+            print("User {} does not exist! Banned?".format(data_packet[1]))
+            user_data.remove(data_packet) #stop tracking them and remove all data
+        write_data()
+
+init()
+while True:
+    main()
+
+
